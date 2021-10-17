4855917aefccc7cb8d0e54c60b02da202487c52b
Amartya Mondal <atm1504.in@gmail.com>
2021-10-17 19:23:56 +0530

    Addition of layers

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import classes from "./BuildControls.css";
+import { BuildControl } from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: "salad" },
+    { label: 'Bacon', type: "bacon" },
+    { label: 'Cheese', type: "cheese" },
+    { label: 'Meat', type: "meat" },
+];
+
 
 export const BuildControls = (props) => {
     return (
-        <div>
-            
+        <div className={classes.BuildControls}>
+
+            {controls.map(ctrl => (
+                <BuildControl
+                    key={ctrl.label}
+                    label={ctrl.label}
+                    type={ctrl.type}
+                    added={() => props.ingredientAdded(ctrl.type)}
+                />
+            ))}
         </div>
     )
 }
